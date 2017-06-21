3987c572e85825ed9aa9c86d80b4aab172760f64
Kaqtus <vahanko@gmail.com>
2017-06-21 09:26:35 +0200

    dodanie spisu stron

--- giapDjango/urls.py
+++ giapDjango/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^main/', include('main.urls')),
     url(r'^skanowanie_laserowe/', include('skanowanie_laserowe.urls')),
     url(r'^administracja_publiczna/', include('administracja_publiczna.urls')),
 ]
---
+++ main/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# from django.utils import timezone
+
+
+def index(request):
+    return render(request, 'main/index.html', {})
