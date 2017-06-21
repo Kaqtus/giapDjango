cdf4aeadd2640b35db8bb6ed4b47006564ee2c56
Kaqtus <Kaqtus@24dcaea7d349>
2017-06-21 05:58:31 +0000

    dostosowanie do PE

--- giapDjango/settings.py
+++ giapDjango/settings.py
@@ -25,9 +25,10 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 SECRET_KEY = 'i3@bt4w2e$%-g7sgux_i9*_&!7(%g1v7=mxhmupgejm#_8*)=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,10 +129,11 @@ DATE_INPUT_FORMATS = '%d-%m-%Y'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
