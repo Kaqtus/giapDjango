b5016f8487e53922c1cc77faad085dc4323192a3
Kaqtus <vahanko@gmail.com>
2017-06-21 09:27:11 +0200

    dodanie spisu stron

--- giapDjango/settings.py
+++ giapDjango/settings.py
@@ -129,11 +129,11 @@ DATE_INPUT_FORMATS = '%d-%m-%Y'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
